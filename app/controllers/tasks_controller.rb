a7b621fc60ce241c7872ae857d8d0a92fa07a37d
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-18 18:05:04 -0400

    show task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,14 +10,15 @@ class TasksController < ApplicationController
   end
 
   def create
-    binding.pry
     task = Task.new({
       title: params[:task][:title],
       description: params[:task][:description]
     })
-    binding.pry
     task.save
-    binding.pry
     redirect_to '/tasks'
+  end
+
+  def show
+    @task = Task.find(params[:id])
   end
 end
